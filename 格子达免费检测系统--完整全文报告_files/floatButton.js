5eea65de7a192334642289542531ab75a3809ebf
bo.liu <bo.liu@boldseas.com>
2016-05-20 15:52:27 +0800

    gezida and my lunwen

---
+++ 格子达免费检测系统--完整全文报告_files/floatButton.js
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html>
+<head><title>503 Service Temporarily Unavailable</title></head>
+<body bgcolor="white">
+<h1>503 Service Temporarily Unavailable</h1>
+<p>The server is temporarily unable to service your request due to maintenance downtime or capacity problems. Please try again later. Sorry for the inconvenience.<br/>
+Please report this message and include the following information to us.<br/>
+Thank you very much!</p>
+<table>
+<tr>
+<td>URL:</td>
+<td>http://chat80.live800.com/live800/chatClient/floatButton.js?jid=8133986536&amp;companyID=511718&amp;configID=123650&amp;codeType=custom</td>
+</tr>
+<tr>
+<td>Server:</td>
+<td>ali-v2-c80</td>
+</tr>
+<tr>
+<td>Date:</td>
+<td>2016/05/17 14:01:34</td>
+</tr>
+</table>
+<hr/>Powered by Tengine</body>
+</html>
---
+++ 格子达免费检测系统--完整全文报告_files/monitor.js
@@ -0,0 +1,37 @@
+try{document.open();}catch(e){;}
+var live800_companyID="511718";
+var releaseVersion="jy";
+var jsessionId = ";jsessionid=1FD4BE2F9752062613EB70C087405452";
+try{
+if(navigator.cookieEnabled){jsessionId = "";}
+}catch(e){}
+var enterurl = null;
+enterurl="http%3A%2F%2Fwww.gezida.com%2FcheckReport%2FfullReport.do%3Fdocid%3D75A5EF483D0C4599B558EC456C62B6A2%26reportType%3D1";
+var isOldSkin=false;
+var server_prefix_list=['http://chat80.live800.com/live800'];
+var isNeedCheckDomainBinding=false;
+var globalWindowAttribute='toolbar=0,scrollbars=0,location=0,menubar=0,resizable=1,width=920,height=620';
+live800_chatVersion="5";
+jid="8133986536";
+var live800_baseUrl="chat80.live800.com";
+var live800_baseHtmlUrl="chat80.live800.com";
+var live800_baseWebApp="/live800";
+var live800_baseChatHtmlDir="/chatClient";
+live800_Language="zh";
+monitor_live800_configID="123649";
+var onlyShowInvite=1;
+var live800_status="1";
+var monitorInterval=15000;
+live800_chatSkinColor ="#39c4bd";
+monitor_live800_configContent="pageChatVisible=0&live800_invite_style=1&live800_invite_top=1&sideSpace=50&posSide=right&live800_invite_title=%26%2336992%3B%26%2335831%3B";
+live800_inviteContent="&#35831;&#38382;&#26377;&#20160;&#20040;&#21487;&#20197;&#24110;&#24744;?";
+live800_visitorAddr="&#27426;&#36814;&#24744;&#65292;&#26469;&#33258; &#21271;&#20140;&#24066; &#30340;&#26379;&#21451;";
+var invite_accept='&#25509;&#12288;&#21463;';
+var invite_cancel='&#24573;&#12288;&#30053;';
+var innerpage_chat='&#28857;&#20987;&#21672;&#35810;';
+var innerpage_leavemessage='&#28857;&#20987;&#30041;&#35328;';
+var default_exit_words='您确定您要退出吗?';
+var init_title='按住鼠标,你可以拖动我哟!';
+var mobile_icons_cfg='icon_color=%23FE7E1D&icon_tel_open=0&tel_number=028-88888888&icon_position=b&icon_chat_mode=1&icon_isOpen=0&icon_tel_words=%26%2330005%3B%26%2335805%3B%26%2320132%3B%26%2327969%3B&icon_chat_words=%26%2322312%3B%26%2332447%3B%26%2323458%3B%26%2326381%3B&icon_off_line_words=离线留言';
+var isChatWindowViaSSL=false;
+document.write("<scr"+"ipt language=\"javascript\" src=\"http://chat80.live800.com/live800/chatClient/monitorStatic5.js\"></scr"+"ipt>");
